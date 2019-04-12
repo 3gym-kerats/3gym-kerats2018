b8280f7f908e74c0f98eec9b583ce9866ea8f774
kgiannaras <38177496+kgiannaras@users.noreply.github.com>
2019-04-12 21:44:07 +0300

    Update cane-distance-noserial.ino

--- arduino-code/cane-distance-noserial/cane-distance-noserial.ino
+++ arduino-code/cane-distance-noserial/cane-distance-noserial.ino
@@ -1,73 +1,86 @@
-int trigPin1 = 2;
-int echoPin1 = 3;
+#include <Servo.h>
 
-int trigPin2 = 5;
-int echoPin2 = 6;
+Servo myservo;  // δημιουργήστε ένα αντικείμενο myservo για να ελέγχεται το servo motor
+//  6 αντικείμενα servo μπορούνα δημιουργηθούν στις περισσότερες πλακέτες
+
+int pos = 0;    // μεταβλητή για την αποθήκευση της θέσης servo
+int trigPin1 = 9;
+int echoPin1 = 10;
+int trigPin2 = 12;
+int echoPin2 = 11;
+int duration = 100 ;
 
 void setup() {
+    myservo.attach(3);  //συνδέει το servo στον ακροδέκτη 3 με το αντικείμενο servo
   
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
-
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
-
 }
 
-void beepFirst(){
-   // play a note on pin 10 
-  tone(10, 340, 100);
-  delay(30);
-  // turn off tone function for pin 10:
-  noTone(10);
+void beep(){
+   // παίξε έναν τόνο στον ακροδέκτη 13
+  tone(13, 340, duration);
+  delay(duration);
+  // απενεργοποιήστε τη λειτουργία τόνου για τον ακροδέκτη 13
+  noTone(13);
 }
 
-void beepSecond(){
-   // play a note on pin 10 
-  tone(10, 240, 100);
-  delay(20);
-  // turn off tone function for pin 10:
-  noTone(10);
+void servomove(){
+for (pos = 0; pos <= 180; pos += 10) { // πηγαίνει από 0 μοίρες έως 180 μοίρες
+    // σε βήματα 1 βαθμού
+    myservo.write(pos);              // πείτε στο σερβο να πάει στη θέση του σε μεταβλητή 'pos'
+    delay(5);                       // περιμένει 15ms για το σερβο να φτάσει στη θέση
+  }
+  for (pos = 180; pos >= 0; pos -= 10) { // πηγαίνει από 180 μοίρες έως 0 μοίρες
+    myservo.write(pos);              // πείτε στο σερβο να πάει στη θέση του σε μεταβλητή 'pos'
+    delay(5);                       // περιμένει 15ms για το σερβο να φτάσει στη θέση
+  }
 }
 
-
-void firstsensor(){ // This function is for first sensor.
+void sensor1(){ // Αυτή η λειτουργία είναι για τον πρώτο αισθητήρα , των τοίχων
   int duration1, distance1;
   digitalWrite (trigPin1, HIGH);
   delayMicroseconds (10);
   digitalWrite (trigPin1, LOW);
   duration1 = pulseIn (echoPin1, HIGH);
   distance1 = (duration1/2) / 29.1;
-
-
-  if (distance1 >=65) {  // Change the number for long or short distances.
-     beepFirst();
-  } else {
-//-----
-  }   
+     
+  if (distance1 <=60 && distance1 >=5) {  // Αλλάξτε τον αριθμό για μεγάλες ή μικρές αποστάσεις.                                                                                                                                                                                                                             
+     servomove(); 
+    } else {
+      // Μην κάνεις  τίποτα
+  }
 }
 
-void secondsensor(){ // This function is for second sensor.
+void sensor2(){ //Αυτή η λειτουργία είναι για τον δεύτερο αισθητήρα , του πεζοδρόμιου
     int duration2, distance2;
     digitalWrite (trigPin2, HIGH);
     delayMicroseconds (10);
     digitalWrite (trigPin2, LOW);
     duration2 = pulseIn (echoPin2, HIGH);
     distance2 = (duration2/2) / 29.1;
-
-  
-    if (distance2 <=60) {  // Change the number for long or short distances.
-     beepSecond();
-    }
- else {
-//-----
-    }   
-}
-
+       
+    if (distance2 <=15 && distance2 >=1) {  // Αλλάξτε τον αριθμό για μεγάλες ή μικρές αποστάσεις.
+     duration = 800;
+     beep();
+    } else if (distance2 <=40 && distance2 >=16) {  // Αλλάξτε τον αριθμό για μεγάλες ή μικρές αποστάσεις.
+      delay(250);
+     duration = 300;
+     beep();   
+     } else if (distance2 <=65 && distance2 >=41) {  // Αλλάξτε τον αριθμό για μεγάλες ή μικρές αποστάσεις.
+      delay(500);
+     duration = 50;
+     beep();    
+    } else {
+      // Μην κάνεις τίποτα
+  }  
+ }
+ 
 void loop() {
-  
-firstsensor();
+sensor1();
 delay(150);
-secondsensor();
+sensor2();
 delay(150);
 }
